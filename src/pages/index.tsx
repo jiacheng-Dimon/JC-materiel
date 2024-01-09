926bbbda60c4595f93da0e0825b45745c782226f
zhangjiacheng.zz <zhangjiacheng.zz@bytedance.com>
2024-01-09 17:22:51 +0800

    feat: 初始化pnpm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -103,7 +103,7 @@ export default function Home() {
               Deploy <span>-&gt;</span>
             </h2>
             <p>
-              Instantly deploy your Next.js site to a shareable URL
+              Instantly deploy your Next.js site to a shar2222eable URL
               with&nbsp;Vercel.
             </p>
           </a>
